dd22459165d1178c1524876b909800ddf3e8f135
quoc95 <quoc95pro@gmail.com>
2019-01-04 22:02:05 +0700

    04-01-2019_2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,39 +1,68 @@
 var theToggle = document.getElementById('toggle-header');
-          var menu = document.getElementById('menu-header');
-      
-      // hasClass
-      function hasClass(elem, className) {
-        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-      }
-      
-      // toggleClass
-      function toggleClass(elem, className) {
-        var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
-          if (hasClass(elem, className)) {
-              while (newClass.indexOf(" " + className + " ") >= 0 ) {
-                  newClass = newClass.replace( " " + className + " " , " " );
-              }
-              elem.className = newClass.replace(/^\s+|\s+$/g, '');
-          } else {
-              elem.className += ' ' + className;
-          }
-      }
-      
-      theToggle.onclick = function() {
-         toggleClass(this, 'on');
-         toggleClass(menu, 'menu-header-toggled');
-         
-         return false;
-      }
-      
-      function toggleMenu(elem){
-        
-        if (hasClass(elem, 'on')) {
-          while (newClass.indexOf(" " + className + " ") >= 0 ) {
-              newClass = newClass.replace( " " + className + " " , " " );
-          }
-          elem.className = newClass.replace(/^\s+|\s+$/g, '');
-      } else {
-          elem.className += ' ' + className;
-      }
-      }
+var menu = document.getElementById('menu-header');
+
+// hasClass
+function hasClass(elem, className) {
+    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+
+// toggleClass
+function toggleClass(elem, className) {
+    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, " ") + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0) {
+            newClass = newClass.replace(" " + className + " ", " ");
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function () {
+    toggleClass(this, 'on');
+    toggleClass(menu, 'menu-header-toggled');
+
+    return false;
+}
+
+function toggleMenu(elem) {
+
+    if (hasClass(elem, 'on')) {
+        while (newClass.indexOf(" " + className + " ") >= 0) {
+            newClass = newClass.replace(" " + className + " ", " ");
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+// right menu category
+
+$(function () {
+    var Accordion = function (el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
+
+        // Variables privadas
+        var links = this.el.find('.link');
+        // Evento
+        links.on('click', { el: this.el, multiple: this.multiple }, this.dropdown)
+    }
+
+    Accordion.prototype.dropdown = function (e) {
+        var $el = e.data.el;
+        $this = $(this),
+            $next = $this.next();
+
+        $next.slideToggle();
+        $this.parent().toggleClass('open');
+
+        if (!e.data.multiple) {
+            $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+        };
+    }
+
+    var accordion = new Accordion($('#accordion'), false);
+});
