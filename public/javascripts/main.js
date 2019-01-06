390c230b663c04e99ec54dc0e27bf55b7e940e8a
quoc95 <quoc95pro@gmail.com>
2019-01-06 20:35:33 +0700

    06-01-2019

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -66,3 +66,13 @@ $(function () {
 
     var accordion = new Accordion($('#accordion'), false);
 });
+
+$(document).on('ready', function() {
+    $(".vertical-center-4").slick({
+      dots: true,
+      vertical: true,
+      centerMode: true,
+      slidesToShow: 4,
+      slidesToScroll: 2
+    });
+});
