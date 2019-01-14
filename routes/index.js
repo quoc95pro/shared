35b482063a1f250c0fb5c3a6a97e787269ed3bf0
quoc95 <quoc95pro@gmail.com>
2019-01-14 21:46:05 +0700

    14-01-2019_2

--- routes/index.js
+++ routes/index.js
@@ -4,48 +4,64 @@ var Game = require('../models/game');
 var moment = require('moment');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   Game.find({}, (err, game) => {
-    if(err){
+    if (err) {
       console.log(err);
     }
-    res.render('public/index', { title: 'Index',game: game, moment: moment });
+    res.render('public/index', { title: 'Index', game: game, moment: moment });
   });
 });
 // Data test
-router.get('/data', function(req, res, next) {
-    Game.find({}, (error, data)=>{
-        if(error){
-            console.log(error);
-        }
-        res.send(data);
-    });
+router.get('/data', function (req, res, next) {
+  Game.find({}, (error, data) => {
+    if (error) {
+      console.log(error);
+    }
+    res.send(data);
+  });
 });
 
-router.get('/detail/:id', function(req, res){
-  var a = '';
-    var b = '';
-  if(res.locals.cate !=''){
-    var a = 'category';
-    var b = res.locals.cate;
-  }
-  Game.findById(req.params.id, function(err, game){
-    if(err){
+router.get('/detail/:id', function (req, res) {
+  Game.findById(req.params.id, function (err, game) {
+    if (err) {
       console.log(err);
     }
-
-    res.render('public/detail', {game: game, moment: moment,breadcrumbs : a, link : b });
+    res.render('public/detail', { game: game, moment: moment });
   })
 });
 
-router.get('/category/:cate', function(req, res){
-  req.flash('cate', req.params.cate);
-Game.getGameByCategory(req.params.cate, function(err, game){
-  if(err){
-    console.log(err);
-  }  
-  res.render('public/category', {game: game, cate:req.params.cate});
-})
+router.get('/countViews/:id', function (req, res) {
+  var id = req.params.id;
+  Game.getGameById(id, function (err, game) {
+    if (err) res.send({ errors: err });
+    game.views = game.views + 1;
+    Game.updateGame(id, game, (err, g) => {
+      if (err) res.send({ errors: err });
+      res.send({ success_msg: 'Edit success' });
+    });
+  });
+});
+
+router.get('/category/:cate', function (req, res) {
+  Game.getGameByCategory(req.params.cate, function (err, game) {
+    if (err) {
+      console.log(err);
+    }
+    res.render('public/category', { game: game, cate: req.params.cate });
+  })
+});
+
+router.get('/search/:name', function (req, res) {
+  var id = req.params.id;
+  Game.getGameById(id, function (err, game) {
+    if (err) res.send({ errors: err });
+    game.views = game.views + 1;
+    Game.updateGame(id, game, (err, g) => {
+      if (err) res.send({ errors: err });
+      res.send({ success_msg: 'Edit success' });
+    });
+  });
 });
 
 module.exports = router;
