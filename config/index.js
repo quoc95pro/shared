5367fa4ba40bc9a0f103f7900aa2045ff30d86ec
quoc95 <quoc95pro@gmail.com>
2019-02-12 16:50:44 +0700

    12-02-2019_5

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ var configValues = require("./config");
 
 module.exports = {
     getDBConnectionString: function(){
-        //  return `mongodb://${configValues.username}:${configValues.password}@ds153093.mlab.com:53093/shared`;
-        return `mongodb://${configValues.username}:${configValues.password}@localhost:27017/shared?authSource=admin`;
+          return `mongodb://${configValues.username}:${configValues.password}@ds153093.mlab.com:53093/shared`;
+        //return `mongodb://${configValues.username}:${configValues.password}@localhost:27017/shared?authSource=admin`;
     }
 }
