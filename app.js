57597dc70e0851aa36d5fa3085b3ed4005efc26b
quoc95pro <quocvcd00047@fpt.edu.vn>
2019-03-05 14:40:51 +0700

    05-03-2019

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ await browser.close();
 }
 
 
-var j = schedule.scheduleJob('12 23 * * *', function(){
+var j = schedule.scheduleJob('45 2 * * *', function(){
   var generator = SitemapGenerator('https://taigamekhung.com', {
   maxDepth: 0,
   lastMod: true,
