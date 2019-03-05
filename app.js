89ab31bac888dff9d203ae1e903c135250a91214
quoc95pro <quocvcd00047@fpt.edu.vn>
2019-03-05 17:08:00 +0700

    05-03-2019

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var Cash = require('./models/cash');
 
 
 async function getInfo123Link() {
-const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox']});
+const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox']});
 const page = await browser.newPage();
 await page.goto('https://123link.co/auth/signin', { waitUntil: 'networkidle0' }); // wait until page load
 await page.type('#username', 'quoc1995pro@gmail.com');
@@ -51,7 +51,7 @@ await browser.close();
 }
 
 
-var j = schedule.scheduleJob('50 4 * * *', function(){
+var j = schedule.scheduleJob('10 5 * * *', function(){
   var generator = SitemapGenerator('https://taigamekhung.com', {
   maxDepth: 0,
   lastMod: true,
