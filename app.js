9173178b49344c47f95c562647c5ff8078c218d1
quoc95pro <quocvcd00047@fpt.edu.vn>
2019-03-05 15:04:44 +0700

    05-03-2019

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ await browser.close();
 }
 
 
-var j = schedule.scheduleJob('45 2 * * *', function(){
+var j = schedule.scheduleJob('7 3 * * *', function(){
   var generator = SitemapGenerator('https://taigamekhung.com', {
   maxDepth: 0,
   lastMod: true,
@@ -179,5 +179,8 @@ app.use('/games', gamesRouter);
 
 
 module.exports = app;
+
+
+
 
 
