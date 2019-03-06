dada25b079493fa36f25d95b6d218a081aa66ac5
quoc95 <quoc95pro@gmail.com>
2019-03-06 16:07:14 +0700

    06-03-2019

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ await browser.close();
 }
 
 
-var j = schedule.scheduleJob('6 4 * * *', function(){
+var j = schedule.scheduleJob('8 4 * * *', function(){
   var generator = SitemapGenerator('https://taigamekhung.com', {
   maxDepth: 0,
   lastMod: true,
