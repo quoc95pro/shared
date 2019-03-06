bcfc35a203ba7028db66ad1a6975ba04fb4b6e50
quoc95 <quoc95pro@gmail.com>
2019-03-06 15:49:07 +0700

    06-03-2019

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ await browser.close();
 }
 
 
-var j = schedule.scheduleJob('49 22 * * *', function(){
+var j = schedule.scheduleJob('50 3 * * *', function(){
   var generator = SitemapGenerator('https://taigamekhung.com', {
   maxDepth: 0,
   lastMod: true,
